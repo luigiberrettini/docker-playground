c6745096a53c368c60367c61e418368b0cdfbe1b
Luigi Berrettini <luigi.berrettini@gmail.com>
2016-08-09 14:36:02 +0200

    Add commons issues

--- tutorial.sh
+++ tutorial.sh
@@ -13,6 +13,35 @@ mkdir --parent ~/workdir
 
 
 
+### Issue - PID 1 zombie reaping
+http://blog.phusion.nl/2015/01/20/docker-and-the-pid-1-zombie-reaping-problem/
+http://engineeringblog.yelp.com/2016/01/dumb-init-an-init-for-docker.html/
+http://github.com/Yelp/dumb-init
+http://github.com/discordianfish/alpine-armhf-docker-dumb-init/blob/master/Dockerfile
+
+
+
+### Issue - Volumes and permissions
+http://twitter.com/luigiberrettini/status/735730691724304384
+
+http://github.com/rocker-org/rocker/wiki/Sharing-files-with-host-machine
+http://github.com/rocker-org/rocker/wiki/managing-users-in-docker
+http://denibertovic.com/posts/handling-permissions-with-docker-volumes
+
+http://github.com/docker/docker/issues/7198#issuecomment-112015480
+# We've worked around this issue in a build setting by having the Docker container chown the build output
+
+https://github.com/kubernetes/kubernetes/issues/2630#issuecomment-110028095
+#@thockin @pmorie The syntax for setting gid is --user "uid:gid". For e.g.
+
+http://renzok.github.io/2015/09/20/docker-host-volume-user-permissions.html
+
+http://github.com/kylemanna/docker-aosp/blob/master/utils/docker_entrypoint.sh
+
+http://github.com/dotnet/cli/blob/rel/1.0.0/scripts/docker/centos/Dockerfile
+
+
+
 ### Single Docker engine - Build and run PHP
 printf '<html>\n    <head><title>Hello World</title></head>\n    <body><h1>Hello World! <?php phpinfo(); ?></h1></body>\n</html>\n' > ~/workdir/index.php
 
